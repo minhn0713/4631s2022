d32e88cb7ed9b00a8311b358de11a758db8ee74e
Minh Nguyen <minhnguyen130798@gmail.com>
2022-03-02 17:31:37 -0500

    week5

---
+++ New_World_Progress_By_Week/week5/app/src/main/java/com/example/news_app/chat_opt_auth_2.java
@@ -0,0 +1,14 @@
+package com.example.news_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class chat_opt_auth_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_opt_auth2);
+    }
+}
